f63282e97a215e4659a66e9c395eb632c69f63ed
Chen Wang <briannawangchen01@gmail.com>
2022-02-01 22:15:34 -0500

    algorithm improve

--- Development Code/src/component/Level1.js
+++ Development Code/src/component/Level1.js
@@ -1,17 +1,22 @@
 import React from "react";
 import HeaderLevel from "./HeaderLevel";
 import BottomInstructions from "./BottomInstructions";
-import MergeSort from "../algorithm/MergeSort";
+import MergeSortComponent from "../algorithm/MergeSortComponent";
 
 function Level1() {
+  
   return (
     <div>
       <HeaderLevel />
       <BottomInstructions />
+      <MergeSortComponent />
 
       <div id="result"></div>
       <div>This is Level1</div>
-      {/*<MergeSort />*/}
+     
+      <div id="left"></div>
+     
+
       
     </div>
   );
