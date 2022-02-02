e79167a3aeb202f02886167173d97c33c6586109
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2022-02-01 20:24:39 -0500

    added sub-level selection dropdown menu & random number array generating function

--- Development Code/src/component/HeaderLevel.js
+++ Development Code/src/component/HeaderLevel.js
@@ -1,25 +1,56 @@
 import React, { useState } from "react";
 import "./HeaderLevel.css";
 import { Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export default function HeaderLevel() {
-  const [currentProblem, setCurrentProblem] = useState([]);
-  const [arrayLength, setArrayLength] = useState(10);
+  // const [currentProblem, setCurrentProblem] = useState([]);
+  // const [arrayLength, setArrayLength] = useState(10);
 
-  let newArray = [];
-  let random = new Set();
+  const [ArrayLength, setArrayLength] = useState(10); //the deafult array length is 10 
+  const [NumberArray, setNumberArray] = useState([]);
 
-  const newProblems = () => {
-    while (random.size < arrayLength) {
-      random.add(Math.floor(Math.random() * 20) + 1);
-    }
-    newArray = [...random];
-    setCurrentProblem(newArray);
-    console.log(currentProblem);
+  let newArray;
+  
+  //TODO: better to generate unique numbers!!!
+  function generateNumberArray() {
+    // setNumberArray(
+    //   Array.from({ length: {ArrayLength} }, () => Math.floor(Math.random() * 40))
+    // );
+    newArray = Array.from({ length: ArrayLength }, () =>
+      Math.floor(Math.random() * 20) //numbers range from 0-20
+    );
+  }
+
+  // let newArray = [];
+  // let random = new Set();
+ 
+  //TODO: when 'RUN' btn is clicked for the first time, we get an empty array??? 
+  const newProblems = (e) => {
+    generateNumberArray();
+    setNumberArray(newArray);
+    console.log(NumberArray);
+    // while (random.size < arrayLength) {
+    //   random.add(Math.floor(Math.random() * 20) + 1);
+    // }
+    // newArray = [...random];
+    // setCurrentProblem(newArray);
+    // console.log(currentProblem);
   };
+
   return (
     <div className="HeaderLevel">
-      <div className="HeaderLevel__chooseLevel">
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Select Level
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="/level1">Level1</Dropdown.Item>
+          <Dropdown.Item href="/level2">Level2</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      {/* <div className="HeaderLevel__chooseLevel">
         <label for="levels">Level:</label>
         <select
           name="levels"
@@ -33,7 +64,7 @@ export default function HeaderLevel() {
           <option value="10">Level 3</option>
           <option value="50">Level 4</option>
         </select>
-      </div>
+      </div> */}
 
       <div className="HeaderLevel__runButton">
         <span>
