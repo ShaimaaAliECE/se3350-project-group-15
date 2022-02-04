4590f3f09bb27ce6bb47dcbbe502fa08a2add1ce
Chen Wang <briannawangchen01@gmail.com>
2022-02-04 00:32:03 -0500

    first part comment of algorithm

--- Development Code/src/component/SortingService.js
+++ Development Code/src/component/SortingService.js
@@ -1,23 +1,30 @@
-
 import { Partition } from './Partition';
 
 export class SortingService {
   partitions = [];
-  
+
+
   mergeSort(currentPartition) {
+    //check if there is only one left in the current partition
     if (currentPartition.isSingleItemList()) {
       return currentPartition;
     }
 
+    //if there are more than one, split the partition to left and right
     let left = currentPartition.getLeftHalf();
     let right = currentPartition.getRightHalf();
 
+    //build the left part of the split
     this.buildModel(left, {left: [...currentPartition.items]});
-    this.buildModel(right, {left: [...currentPartition.items]});  
-    
+
+    //build the right part of the split
+    this.buildModel(right, {left: [...currentPartition.items]}); 
+
+    //return left and right together
     return this.merge(this.mergeSort(left), this.mergeSort(right));
   }
 
+  //compare left half and the right half
   merge(left, right){
     let result = [];
     let parts = {left: [...left.items], right: [...right.items]};
@@ -40,24 +47,25 @@ export class SortingService {
     return newPartition;
   }
 
+  //build the left/right part on the webpage
   buildModel(res, part) {
     let nodeIndex = this.partitions.findIndex(p => p.parentId === res.parentId);
    
     if(nodeIndex >= 0) {
       this.partitions[nodeIndex].fragments.push(res.items.slice());
       this.partitions[nodeIndex].part1 = [];
-    
+      
+    //print the split result
       this.partitions[nodeIndex].part2 = part.left;
-      this.partitions[nodeIndex].descr = '*split from ';
-      this.partitions[nodeIndex].show = 'hide';
+      this.partitions[nodeIndex].descr = '   split result  ------>  ';
     }  
     else {
       let node = {parentId: res.parentId, fragments: []};
       
+       //print the merged result
       node.part1 = part.left;
-      node.descr = '*merged from ';
+      node.descr = '   ------>merged   ';
       node.part2 = part.right;
-      node.show = 'group';
       
       node.fragments.push(res.items.slice());
       this.partitions.push(node);
