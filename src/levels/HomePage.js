84d5165f9388a65bed7e06abe86b5e84dc79f332
Taiyi Xu <txu259@uwo.ca>
2022-03-04 20:46:07 -0500

    clock added to mainpage

--- src/levels/HomePage.js
+++ src/levels/HomePage.js
@@ -1,26 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Background from "../assets/css/Background.css"
+import actualTime from '../components/Clock.js'
 
 export default function LevelMain() {
     return (
-        <div className="LevelMain">
-            <h1>LevelMain</h1>
-            <li>
-                <Link to={"/level1"} className="level1">
-                    Level1
-                </Link>
-            </li>
-            <li>
-                <Link to={"/level2"} className="level2">
-                    Level2
-                </Link>
-            </li>
-            <li>
-                <Link to={"/level3"} className="level3">
-                    Level3
-                </Link>
-            </li>
+        <div>
+            <div className="LevelMain">
+                <h1>Welcome My Friend</h1>
+                <li>
+                    <Link to={"/level1"} className="level1">
+                        LVL: Beginner
+                    </Link>
+                </li>
+                <li>
+                    <Link to={"/level2"} className="level2">
+                        LVL: Normal
+                    </Link>
+                </li>
+                <li>
+                    <Link to={"/level3"} className="level3">
+                        LVL:  On your Own
+                    </Link>
+                </li>
+            </div>
+
+            <div>
+                <div className='clock' >
+                    <div className='hours'/>
+                    <div className='minutes'/> 
+                    <div className='seconds'/>
+                </div>
+                
+            </div>
         </div>
+
     );
 }
 
