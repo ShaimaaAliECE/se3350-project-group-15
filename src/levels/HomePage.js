46892105c15f2d8202b5e29eaa279a56bb3a3291
Brianna0510 <65310388+Brianna0510@users.noreply.github.com>
2022-03-04 19:59:09 -0500

    mainpage css

--- src/levels/HomePage.js
+++ src/levels/HomePage.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./HomePage.css";
 export default function LevelMain() {
     return (
         <div className="LevelMain">
-            <h1>LevelMain</h1>
-            <li>
+      
+            <div id="One">
                 <Link to={"/level1"} className="level1">
                     Level1
                 </Link>
-            </li>
-            <li>
+            </div>
+            <div id="Two">
                 <Link to={"/level2"} className="level2">
                     Level2
                 </Link>
-            </li>
-            <li>
+            </div>
+            <div id="Three">
                 <Link to={"/level3"} className="level3">
                     Level3
                 </Link>
-            </li>
+            </div>
         </div>
     );
 }
