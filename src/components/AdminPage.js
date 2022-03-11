c575888cc26c23fee59abfbfd93c138558c39f6a
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2022-03-11 17:46:17 -0500

    Update AdminPage.js

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Card from "react-bootstrap/Card";
 
 export default function AdminPage() {
   return (
-    <div>AdminPage
-        <div>view users</div>
-        
+    <div>
+      <h1>AdminPage</h1>
+      <h2>(:maybe come up with a better design:)</h2>
+      <h2>maybe display player data in 3 cols</h2>
+      {[
+        "Primary",
+        "Secondary",
+        "Success",
+        "Danger",
+        "Warning",
+        "Info",
+        "Light",
+        "Dark",
+      ].map((variant, idx) => (
+        <Card
+          bg={variant.toLowerCase()}
+          key={idx}
+          text={variant.toLowerCase() === "light" ? "dark" : "white"}
+          style={{ width: "18rem" }}
+          className="mb-2"
+        >
+          <Card.Header>Ouda</Card.Header>
+          <Card.Body>
+            <Card.Title>{variant} Card Title </Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
-  )
+  );
 }
-
