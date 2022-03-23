77c1ee2472b2b7f2e7d32f0c714048a038b30301
Taiyi Xu <txu259@uwo.ca>
2022-03-23 00:46:25 -0400

    search game record function implemented

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,7 +1,9 @@
 import { React, useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
-import { doc, getDoc, collection, getCollection } from "firebase/firestore";
-import firebase, { db } from "../Authentication/firebase";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db } from "../Authentication/firebase";
+
+
 
 export default function AdminPage() {
   // this place is used for add, remove and modify actions of player records.
@@ -9,20 +11,64 @@ export default function AdminPage() {
   //Getting the information of each doc in the collection
 
 
-  const recordDoc = doc(db,'gameRecords','32zL5MbteVo5lY4mFRQI');
-  const [gameData, setGameData] = useState();
+  const recordCollection = collection(db, 'gameRecords');
+  //const [timeSpent, setTimeSpent] = useState('');
 
-  async function readRecordData() {
-    const mySnapshot = await getDoc(recordDoc);
-    if (mySnapshot.exists()) {
-      setGameData(mySnapshot);
-      console.log(mySnapshot);
-      //console.log(gameData);
-    }
+  const [querySnapshotArray, setQuerySnapshotArray] = useState([]);
 
+  // const variant = ["Primary",
+  //   "Secondary",
+  //   "Success",
+  //   "Danger",
+  //   "Warning",
+  //   "Info",
+  //   "Light",
+  //   "Dark"]
+
+  // const gameRecords = {
+  //   id: '',
+  //   dateTime: new Date,
+  //   email: '',
+  //   score: 0,
+  //   timeSpent: ''
+  // };
+
+
+  const [playerEmail, setPlayerEmail] = useState();
+
+  async function readRecordData() { // worked
+    console.log(playerEmail);
+    const targetData = query(recordCollection, where('email', '==', playerEmail));
+    const querySnapshot = await getDocs(targetData);
+    var innerQuery = [];
+    querySnapshot.forEach((doc) => {
+      var eachQuery = [];
+      eachQuery.push(doc.id);
+      eachQuery.push(doc.data().dateTime);
+      eachQuery.push(doc.data().email);
+      eachQuery.push(doc.data().score);
+      eachQuery.push(doc.data().timeSpent);
+      console.log(eachQuery);
+      innerQuery.push(eachQuery);
+    });
+    setQuerySnapshotArray(innerQuery);
+    //setGameRecords(idArray, dateTimeArray, emailArray, scoreArray, timeSpentArray);
   };
 
+  const getPlayerEmail = () => {
+    var email = document.getElementById('email').value;
+    setPlayerEmail(email);
+  };
 
+  // function setGameRecords(id, dateTime, email, score, timeSpent) {
+  //   setId(id)
+  //   setDateTime(dateTime);
+  //   setEmail(email);
+  //   setScore(score);
+  //   setTimeSpent(timeSpent);
+
+  //   //console.log(gameRecords.id)
+  // }
 
 
   // make sure only admin can view this page!
@@ -38,32 +84,34 @@ export default function AdminPage() {
       <h1>AdminPage</h1>
       <h2>(:maybe come up with a better design:)</h2>
       <h2>maybe display player data in 3 cols</h2>
-      {[
-        "Primary",
-        "Secondary",
-        "Success",
-        "Danger",
-        "Warning",
-        "Info",
-        "Light",
-        "Dark",
-      ].map((variant, idx) => (
-        <Card
-          bg={variant.toLowerCase()}
-          key={idx}
-          text={variant.toLowerCase() === "light" ? "dark" : "white"}
-          style={{ width: "18rem" }}
-          className="mb-2"
-        >
-          <Card.Header>Ouda</Card.Header>
-          <Card.Body>
-            <Card.Title>{variant} Card Title </Card.Title>
-            <Card.Text onClick={readRecordData}>
-              Click me
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
+      <div>
+        <input type='text' id='email' onChange={getPlayerEmail} />
+        <button className="btn btn-primary" onClick={readRecordData}>
+          Search
+        </button>
+      </div>
+      {
+        querySnapshotArray.map((item) => {
+          return <Card
+            bg="Primary"
+            key="1"
+            text={"dark"}
+            style={{ width: "18rem" }}
+            className="mb-2"
+          >
+            <Card.Header>{item[0]}</Card.Header>
+            <Card.Body>
+              <Card.Title>{item[1]}</Card.Title>
+              <Card.Text>{item[2]}</Card.Text>
+              <Card.Text>{item[3]}</Card.Text>
+              <Card.Text>{item[4]}</Card.Text>
+            </Card.Body>
+          </Card>
+        }
+        )
+      }
     </div>
+
   );
 }
+
