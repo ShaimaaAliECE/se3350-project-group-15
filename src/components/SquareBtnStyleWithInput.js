24410b4e38b9685fe4067d23ab7a7b7760f18782
Henry Zhang <71099565+HenryCZhang@users.noreply.github.com>
2022-03-23 00:41:41 -0400

    added answer checking sound effect &  visual display

--- src/components/SquareBtnStyleWithInput.js
+++ src/components/SquareBtnStyleWithInput.js
@@ -1,18 +1,29 @@
 import { useAlert } from "react-alert";
+import { useState } from "react";
+import useSound from "use-sound";
+import correctSound from "../assets/sounds/correct.wav";
+import wrongSound from "../assets/sounds/wrong.mp3";
 //From Ives Luo
 
 export default function SquareBtnStyleWithInput(props) {
   const alert = useAlert(); //Henry: fancy alert
+  const [playCorrectSound, setCorrectSound] = useSound(correctSound);
+  const [playWrongSound, setWrongSound] = useSound(wrongSound);
+  const [inputColor,setInputColor] = useState('');
 
   const checkAns = (event) => {
     if (event.target.value === event.target.id) {
       props.setCurrentPoint(props.currentPoint + 1);
       event.target.disabled = true;
+      setInputColor('#b9fbc0');
       alert.success("correct");
+      playCorrectSound();
     } else if (event.target.value === "") {
     } else {
       event.target.value = "";
+      setInputColor('#fe6d73');
       alert.error("wrong answer");
+      playWrongSound();
     }
   };
 
@@ -27,6 +38,7 @@ export default function SquareBtnStyleWithInput(props) {
       id={props.id}
       name="ansBox"
       onBlur={checkAns}
+      style={{backgroundColor: inputColor}}
     />
   );
 }
