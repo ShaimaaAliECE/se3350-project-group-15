3cee3ac41b8b472778fd46d18bb3d9fc4ada747e
Brianna0510 <65310388+Brianna0510@users.noreply.github.com>
2022-03-25 20:51:24 -0400

    alert when 3 errors

--- src/components/SquareBtnStyleWithInput.js
+++ src/components/SquareBtnStyleWithInput.js
@@ -1,10 +1,17 @@
 import { useAlert } from "react-alert";
 import { useEffect } from "react";
+import "./Popup.css";
+
+
+import { Link } from "react-router-dom";
 //From Ives Luo
 export let error = 0;
+export function errorAlert(){
+  alert("You make error 3 times");
+}
 export default function SquareBtnStyleWithInput(props) {
-
   const alert = useAlert(); //Henry: fancy alert
+
   const checkAns = (event) => {
     if (event.target.value === event.target.id) {
       props.setCurrentPoint(props.currentPoint + 1);
@@ -14,8 +21,15 @@ export default function SquareBtnStyleWithInput(props) {
     } else {
       event.target.value = "";
       error++;
-      props.currentError(error);
-      alert.error("wrong answer " + error);
+
+      if (error !== 3) {
+        alert.error("wrong answer " + error);
+      } else {
+        error = 0;
+        errorAlert();
+      }
+
+      // props.currentError(error);
     }
   };
 
@@ -33,4 +47,3 @@ export default function SquareBtnStyleWithInput(props) {
     />
   );
 }
-
--- src/levels/Level2.js
+++ src/levels/Level2.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Helper from "../Helper/Helper";
 import SquareBtnStyle from "../components/SquareBtnStyle";
 import SquareBtnStyleWithInput from "../components/SquareBtnStyleWithInput";
 import LevelControl from "../components/LevelControl";
 import InstructionPanel from "../components/InstructionPanel";
-
+import Popup from "../components/Popup";
 
 const helper = new Helper();
 
-export default function Level2(){
+export default function Level2() {
   const currentLevel = 2;
   const [currentPoint, setCurrentPoint] = React.useState(0);
   const [currentQuestion, setCurrentQuestion] = React.useState([]);
@@ -16,7 +16,7 @@ export default function Level2(){
   const [hasStarted, setHasStarted] = React.useState(false);
   const [currentStep, setCurrentStep] = React.useState(0);
   const displayArray = summaryArray.slice(0, currentStep - 1);
-  const errorCount=0;
+  const errorCount = 0;
 
   const levelStart = () => {
     let generate = helper.generateNumberArray(10, 20);
@@ -25,7 +25,6 @@ export default function Level2(){
     setCurrentStep(1);
     setHasStarted(true);
     setCurrentPoint(10);
-   
   };
 
   const levelRestart = () => {
@@ -50,8 +49,17 @@ export default function Level2(){
       setCurrentPoint(0);
     }
   };
- 
 
+  //for the popup close button test
+  const [buttonPopup, setButtonPopup] = useState(false);
+
+  // const errorPopup=()=>{
+  //   if(errorCount===3){
+  //     ()=>{setButtonPopup(true);}
+
+  //   }
+
+  // };
   return (
     <div className="Level1">
       <h1>Level 2</h1>
@@ -61,12 +69,17 @@ export default function Level2(){
         hasStarted={hasStarted}
         currentLevel={currentLevel}
         currentPoint={currentPoint}
-
       />
+
       <div className="display-area">
         <div className="display-area-row">
           {currentQuestion.map((item, index) => {
-            return <SquareBtnStyle key={index}>{item}</SquareBtnStyle>;
+            return (
+              <div>
+                <SquareBtnStyle key={index}>{item}</SquareBtnStyle>
+                
+              </div>
+            );
           })}
         </div>
         <div className="display-area-dynamic">
@@ -97,7 +110,7 @@ export default function Level2(){
                   {item.map((subItem, j) => (
                     <div className="display-area-col" key={j}>
                       {j === item.length / 2 && <SquareBtnStyle opacity />}
-                      {subItem.map((num, k,errorCount) => (
+                      {subItem.map((num, k, errorCount) => (
                         <SquareBtnStyleWithInput
                           key={k}
                           id={num}
@@ -110,11 +123,14 @@ export default function Level2(){
                     </div>
                   ))}
                 </div>
-
               );
             }
           })}
         </div>
+        {/* <Popup
+                  trigger={buttonPopup} setTrigger={setButtonPopup}>
+                  <h1> my pop up</h1>
+                </Popup> */}
       </div>
 
       <InstructionPanel
